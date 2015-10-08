ded4762779d5a3196df22757d3be9eaa20782861
zjjsven <suniya.zjj@outlook.com>
2015-10-08 11:25:51 +0800

    personal page finished

--- app/js/app.js
+++ app/js/app.js
@@ -1,51 +1,79 @@
 var mainApp = angular.module('mainApp', [
-    'ui.router',
-    'bookListApp',
-    'bookDetailApp',
-    'userApp'
+  'ui.router',
+  'ngMessages',
+  'wu.masonry',
+  'bookListApp',
+  'bookDetailApp',
+  'userApp'
 ]);
 
 mainApp.config(function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/books/popular');
+  $urlRouterProvider.otherwise('/books/popular');
 
-    $stateProvider
-        .state('books', {
-            url: '/books',
-            templateUrl: 'views/main.html'
-        })
-        .state('books.popular', {
-            url: '/popular',
-            templateUrl: 'views/popular.html',
-            controller: 'ShowPopularBooks'
-        })
-        .state('books.allbooks', {
-            url: '/allbooks',
-            templateUrl: 'views/allbooks.html',
-            controller: 'ShowAllBooks'
-        })
-        .state('books.free', {
-            url: '/free',
-            templateUrl: 'views/free.html',
-            controller: 'ShowFreeBooks'
-        })
-        .state('books.mybooks', {
-            url: '/mybooks',
-            templateUrl: 'views/mybooks.html',
-            controller: 'ShowMyBooks'
-        })
-		.state('login', {
-            url: '/login',
-            templateUrl: 'views/login.html',
-            controller: 'LoginCtrl'
-        })
-		.state('register', {
-            url: '/register',
-            templateUrl: 'views/register.html',
-            controller: 'RegCtrl'
-        })
-        .state('books.detail', {
-            url:'/:bookId',
-            templateUrl: 'views/detail.html',
-            controller: 'BookDetailCtrl'
-        });
+  $stateProvider
+    .state('main', {
+      url: '',
+      templateUrl: 'views/main.html'
+    })
+    .state('main.popular', {
+      url: '/books/popular',
+      templateUrl: 'views/popular.html',
+      controller: 'ShowPopularBooks'
+    })
+    .state('main.allbooks', {
+      url: '/books/allbooks',
+      templateUrl: 'views/allbooks.html',
+      controller: 'ShowAllBooks'
+    })
+    .state('main.free', {
+      url: '/books/free',
+      templateUrl: 'views/free.html',
+      controller: 'ShowFreeBooks'
+    })
+    .state('main.liked', {
+      url: '/books/liked',
+      templateUrl: 'views/popular.html',
+      controller: 'ShowPopularBooks'
+    })
+    .state('main.mybooks', {
+      url: '/books/mybooks',
+      templateUrl: 'views/mybooks.html',
+      controller: 'ShowMyBooks'
+    })
+    .state('main.detail', {
+      url: '/books/:bookId',
+      templateUrl: 'views/detail.html',
+      controller: 'BookDetailCtrl'
+    })
+    .state('main.home', {
+      url: '/home',
+      templateUrl: 'views/home.html',
+      controller: 'ShowMyBooks'
+    })
+    .state('main.setting', {
+      url: '/setting',
+      templateUrl: 'views/setting.html',
+      controller: 'ShowMyBooks'
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: 'views/login.html',
+      controller: 'LoginCtrl'
+    })
+    .state('register', {
+      url: '/register',
+      templateUrl: 'views/register.html',
+      controller: 'RegCtrl'
+    });
+});
+
+mainApp.run(function($rootScope) {
+  $rootScope.logInUser = {
+    'name': 'admin',
+    'intrID': 'admin@cn.ibm.com',
+    'likedBooks': [78, 79, 80, 81, 82, 83, 84, 85, 86, 67, 68, 69, 71, 72]
+  };
+  $rootScope.logOut = function logOut() {
+    $rootScope.logInUser.name = '';
+  };
 });
--- app/js/controllers/userCtrl.js
+++ app/js/controllers/userCtrl.js
@@ -1,23 +1,35 @@
-var userApp = angular.module('userApp', []);
-userApp.controller('LoginCtrl', function($scope, $http, $location) {
+var userApp = angular.module('userApp', ['ngMessages']);
+userApp.controller('LoginCtrl', function($scope, $rootScope, $http, $location) {
   $scope.user = {};
   $scope.login = function() {
-    var user = {
-      'intrID': $scope.user.intrID,
-      'pwd': $scope.user.pwd
-    };
-    $http.post('/login', user)
-      .success(function(res) {
-        console.log(res);
-        if (res.isSucc) {
           $location.path('/');
-        }
-      })
-      .error(function(res) {
-        console.log('Error: ' + res);
-      });
+      $rootScope.logInUser.name = $scope.user.intrID;
+      console.log('logInUser.name=' + $rootScope.logInUser.name);
+/*
+    if ($scope.loginForm.$valid) {
+      var user = {
+        'intrID': $scope.user.intrID,
+        'pwd': $scope.user.pwd
+      };
+      $location.path('/');
+      $rootScope.logInUser.name = $scope.user.intrID;
+      $http.post('/login', user)
+        .success(function(res) {
+          console.log(res);
+          if (res.isSucc) {
+            $location.path('/');
+            $rootScope.logInUser.name = $scope.user.intrID;
+          }
+        })
+        .error(function(res) {
+          console.log('Error: ' + res);
+        });
+    } else {
+      console.log('Error: loginForm.$valid = ' + $scope.loginForm.$valid);
+    };*/
   };
 });
+
 
 userApp.controller('RegCtrl', function($scope, $http, $location) {
   $scope.user = {};
