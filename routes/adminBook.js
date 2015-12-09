9d12b80f5c14ee5ccffe8d97862f106ee261806c
zjjsven <suniya.zjj@outlook.com>
2015-12-09 20:27:53 +0800

    fix admin getAllBooks

--- routes/adminBook.js
+++ routes/adminBook.js
@@ -3,35 +3,49 @@ var BookProp = require('../models/BookProp.js');
 var History = require('../models/History.js');
 
 module.exports = function(app) {
-	app.get('/admin/books', function(req, res){
-		Book.find(function(err, books){
-			if(err){
-				console.log("[Query books] DB error !");
-			}else{
-				var jbooks = JSON.parse(JSON.stringify(books));
-				BookProp.find(function(err, booksprop){
-					if(err){
-						console.log("[Query BookProp] DB error !");
-					}else{
-						var jbooksprop = JSON.parse(JSON.stringify(booksprop));
-					}
-
-					for (var i = jbooks.length - 1; i >= 0; i--) {
-						for (var m = jbooksprop.length - 1; m >= 0; m--) {
-							if(jbooksprop[m].isbn == jbooks[i].isbn){
-
-								jbooks[i].basicInfo.push(JSON.parse(JSON.stringify(jbooksprop[m])));
-								break;
-							}
-						};
-					};//for
-					// console.log(jbooks);
-					// console.log(jbooks[0].basicInfo[0].name);
-					res.json(jbooks);
-				});//booksprop
-			}
-		});//books
-
-	});// add one book
+  app.get('/admin/books', function(req, res) {
+    Book.find(function(err, books) {
+      if (err) {
+        console.log("[Query books] DB error !");
+        res.send(err);
+      } else {
+        BookProp.find(function(err, booksprop) {
+          if (err) {
+            console.log("[Query BookProp] DB error !");
+            res.send(err);
+          } else {
+            var newBooks = [];
+            for (var i = 0; i < books.length; i++) {
+              for (var j = 0; j < booksprop.length; j++) {
+                if (books[i].isbn == booksprop[j].isbn) {
+                  var book = {};
+                  console.log(books[i]);
+                  console.log(booksprop[j]);
+                  book.unqId = books[i].unqId;
+                  book.isbn = books[i].isbn;
+                  book.name = books[i].name;
+                  book.status = books[i].status;
+                  book.applyTime = books[i].applyTime;
+                  book.lendTime = books[i].lendTime;
+                  book.borrower = books[i].borrower;
+                  book.desc = booksprop[j].desc;
+                  book.publisher = booksprop[j].publisher;
+                  book.author = booksprop[j].author;
+                  book.pageCount = booksprop[j].pageCount;
+                  book.price = booksprop[j].price;
+                  book.count = booksprop[j].count;
+                  book.image = booksprop[j].image;
+                  newBooks.push(book);
+                  break;
+                };
+              };
+            };
+            res.send(newBooks);
+            console.log(newBooks);
+          };
+        });
+      }; //booksprop
+    }); //books
+  }); // add one book
 
 };
--- routes/userBook.js
+++ routes/userBook.js
@@ -3,8 +3,44 @@ var BookProp = require('../models/BookProp.js');
 
 module.exports = function(app) {
   app.get('/books', function(req, res) {
-    Book.find(function(err, books){
-      res.send(books);
+    Book.find(function(err, books) {
+      if (err) {
+        console.log("[Query books] DB error !");
+        res.send(err);
+      } else {
+        BookProp.find(function(err, booksprop) {
+          if (err) {
+            console.log("[Query BookProp] DB error !");
+            res.send(err);
+          } else {
+            var newBooks = [];
+            for (var i = 0; i < books.length; i++) {
+              for (var j = 0; j < booksprop.length; j++) {
+                if (books[i].isbn == booksprop[j].isbn) {
+                  var book = {};
+                  console.log(books[i]);
+                  console.log(booksprop[j]);
+                  book.unqId = books[i].unqId;
+                  book.isbn = books[i].isbn;
+                  book.name = books[i].name;
+                  book.status = books[i].status;
+                  book.desc = booksprop[j].desc;
+                  book.publisher = booksprop[j].publisher;
+                  book.author = booksprop[j].author;
+                  book.pageCount = booksprop[j].pageCount;
+                  book.price = booksprop[j].price;
+                  book.count = booksprop[j].count;
+                  book.image = booksprop[j].image;
+                  newBooks.push(book);
+                  break;
+                };
+              };
+            };
+            res.send(newBooks);
+            console.log(newBooks);
+          };
+        });
+      };
     });
-  })
-}
+  });
+};
