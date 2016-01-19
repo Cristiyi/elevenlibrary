a4ca19b966ed8dca178e635fc083713a39478bcd
zjjsven <suniya.zjj@outlook.com>
2016-01-19 14:35:49 +0800

    fix sort issue

--- app/js/controllers/bookCtrl.js
+++ app/js/controllers/bookCtrl.js
@@ -37,7 +37,10 @@ bookApp.controller('MainBooksCtrl', function($scope, $state, $rootScope, BooksSe
     function sortLikes(a, b) {
       return b.likes.length - a.likes.length;
     };
-    var books = BooksService.books;
+    var books = [];
+    for (var index in BooksService.books){
+      books.push(BooksService.books[index]);
+    };
     $scope.popBooks = books.sort(sortLikes);
   };
 
